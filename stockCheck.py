5ed39ac1937180f996210e13aa70e9f5ae62e674
unknown <mohammedalam52@gmail.com>
2023-07-01 23:08:44 -0400

    pull sku for product

--- stockCheck.py
+++ stockCheck.py
@@ -1,8 +1,20 @@
 import requests 
 import json
 
-r = requests.get(https://shop-us.doverstreetmarket.com/products.json)
+def write_to_file(json_obj):
+    with open("shoe_info.json", "w") as f:
+        json.dump(json_obj, f)
+
+
+r = requests.get("https://shop-us.doverstreetmarket.com/products.json")
 products = json.loads((r.text))['products']
+shoes = []
+final_shoes = {}
 
 for product in products:
-    print(products)
+    if product['product_type'] == "Sneakers":
+        final_shoes['vendor'] = product['vendor']
+        final_shoes['shoe_sizes'] = {variant["title"]: variant["sku"] for variant in product["variants"]}
+
+
+write_to_file(final_shoes)
