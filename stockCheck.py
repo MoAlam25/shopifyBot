a1405544c7499c8d988cd694710ef479debdec1d
unknown <mohammedalam52@gmail.com>
2023-07-03 16:42:15 -0400

    created function to return product url

--- stockCheck.py
+++ stockCheck.py
@@ -1,20 +1,18 @@
 import requests 
 import json
 
-def write_to_file(json_obj):
-    with open("shoe_info.json", "w") as f:
-        json.dump(json_obj, f)
+def stockCheck():
+    r = requests.get("https://shop-us.doverstreetmarket.com/products.json")
+    products = json.loads((r.text))['products']
 
 
-r = requests.get("https://shop-us.doverstreetmarket.com/products.json")
-products = json.loads((r.text))['products']
-shoes = []
-final_shoes = {}
+    for product in products:
+        #print(product['title'])
+        productname = product['title']
 
-for product in products:
-    if product['product_type'] == "Sneakers":
-        final_shoes['vendor'] = product['vendor']
-        final_shoes['shoe_sizes'] = {variant["title"]: variant["sku"] for variant in product["variants"]}
+        if productname == 'Nike - Dunk Low SE NM2 - (FJ4610-702)':
 
-
-write_to_file(final_shoes)
+            producturl = 'https://shop-us.doverstreetmarket.com/products/' + product['handle']
+            return producturl
+        
+    return False
